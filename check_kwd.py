16d6f9b76d213a8896cb56e54aa46d23764d2d49
Markus <markus.krecik@web.de>
2021-09-10 21:43:28 +0200

    cron keyword checker

--- check_kwd.py
+++ check_kwd.py
@@ -1,11 +1,14 @@
 from src.trendsetter import Trendsetter
 import subprocess
 
-kwd = '/m/04n7dpf' # Topic "Interest Rate"
+kwds = {'/m/04n7dpf': 'Interest Rate',
+        '/m/0gz_4': 'Stock market crash'
+       }
 
 ts = Trendsetter()
 
-interest = ts.get_interest(kwd, 'today 3-m')
-if interest.iat[-1,0] > 90:
-    subprocess.run(["/usr/bin/zenity", "--notification",
-                    "--text", "Hot topic: Interest rate"])
+for kwd, val in kwds.items():
+    interest = ts.get_interest(kwd, 'today 3-m')
+    if interest.iat[-1,0] > 90:
+        subprocess.run(["/usr/bin/zenity", "--notification",
+                        "--text", "Hot topic: {}".format(val)])
